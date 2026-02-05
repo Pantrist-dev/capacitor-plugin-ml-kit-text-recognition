05e408c84c8f2ecca66031d3fa25ced6f3700146
NLueg <info@nicolueg.com>
2026-02-05 07:32:14 +0100

    feat(cap-8)!: migrate to capacitor 8

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "PantristCapacitorPluginMlKitTextRecognition",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorPluginMlKitTextRecognitionPlugin",
             targets: ["CapacitorPluginMlKitTextRecognitionPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "6.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "8.0.0")
     ],
     targets: [
         .target(
