bd0f2a4580bbe0fa250a0b6d9931870e14f85242
NLueg <info@nicolueg.com>
2026-09-01 12:26:38 +0200

    chore(deps): some minor dependency updates

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["CapacitorPluginMlKitTextRecognitionPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "8.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "8.5.1")
     ],
     targets: [
         .target(
